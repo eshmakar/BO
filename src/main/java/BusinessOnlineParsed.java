5ac49c949578134b493d2541af9660b2ffaa7d86
eshmakar <59125020+eshmakar@users.noreply.github.com>
2021-11-23 19:19:39 +0300

    Initial commit

---
+++ src/main/java/BusinessOnlineParsed.java
@@ -0,0 +1,66 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.util.ListIterator;
+
+public class BusinessOnlineParsed {
+    private static final String url = "https://m.business-gazeta.ru";
+    private static final String userAgent = "Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36";
+
+    public static void main(String[] args) throws IOException {
+        Document document = Jsoup.connect(url).userAgent(userAgent).get();
+        mainAndHotNews(document);
+        lastNews(document);
+    }
+
+    private static void lastNews(Document document) {
+        int count = 0;
+        String timeLastNews = "time.last-news__time";
+        String lastNews = "a.last-news__link";
+        String lastNewsCommentsCount = "a.last-news__comments";
+
+        ListIterator<Element> times = document.select(timeLastNews).listIterator();
+        ListIterator<Element> lastNewsIterator = document.select(lastNews).listIterator();
+        ListIterator<Element> lastComments = document.select(lastNewsCommentsCount).listIterator();
+
+        while (times.hasNext() && lastNewsIterator.hasNext() && lastComments.hasNext()) {
+            count++;
+            String linkLastNews = document.selectXpath("/html/body/div[1]/section/ul[2]/li[" + count + "]/div/a[1]").toString();
+            System.out.print(times.next().text() + " ");
+            System.out.print(lastNewsIterator.next().text() + " - ");
+            System.out.print(lastComments.next().text() + " - ");
+            System.out.println(url + linkLastNews.substring(9, 21));
+        }
+    }
+
+    private static void mainAndHotNews(Document document) {
+        int count = 0;
+        String glavnayaTema = "h2.article-news__title";
+        String comments = "div.article-news__comments";
+        String linkGlavnaya = document.selectXpath("/html/body/div[1]/article/div/p/a").toString();
+
+
+        System.out.println("Главные темы");
+        System.out.print(document.selectFirst(glavnayaTema).text());
+        System.out.print(" - " + document.selectFirst(comments).text());
+        System.out.println(" - " + url + linkGlavnaya.substring(9, 24)); //ссылка на статью
+
+
+        String hotNews = "a.hot-news__title";
+        String commentsHotNews = "a.hot-news__comments";
+
+        ListIterator<Element> elementListIterator = document.select(hotNews).listIterator();
+        ListIterator<Element> commentsIterator = document.select(commentsHotNews).listIterator();
+
+        while (elementListIterator.hasNext() && commentsIterator.hasNext()) {
+            count++;
+            String linkHotNews = document.selectXpath("/html/body/div[1]/section/ul[1]/li[" + count + "]/div/div/a[1]").toString();
+            System.out.print(elementListIterator.next().text());
+            System.out.print(" - " + commentsIterator.next().text());
+            System.out.println(" - " + url + linkHotNews.substring(9, 21));
+        }
+        System.out.println();
+    }
+}
